cd8e3c829b734d848d66a6842e92d8285784e67e
darvell <andrew@darvell.ru>
2015-01-09 17:46:49 +0300

    First COmmit

---
+++ src/main/java/ru/darvell/meetingserver/TestServ.java
@@ -0,0 +1,29 @@
+package ru.darvell.meetingserver;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Создано для тестов. Потом удалить
+ */
+//@WebServlet("meeting/test")
+public class TestServ extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doGet(req, resp);
+        PrintWriter out = resp.getWriter();
+        out.print("<h1>TEstPage</h1>");
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
